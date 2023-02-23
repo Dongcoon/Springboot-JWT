74b6f492141aff37b447c5fa14155658b3d42662
Kang <gongan1556@naver.com>
2023-02-23 22:32:11 +0900

    Springboot JWT - jwt Filter 등록 테스트

--- src/main/java/com/coon/jwt/config/SecurityConfig.java
+++ src/main/java/com/coon/jwt/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.coon.jwt.config;
 
+import com.coon.jwt.filter.MyFilter1;
+import com.coon.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -16,6 +20,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CorsFilter corsFilter;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // security보다 먼저 동작하려면 addFilterBefore / SecurityContextPersistenceFilter이 가장 먼저
+        http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class); // 먼저 실행 후 2 -> 1
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
